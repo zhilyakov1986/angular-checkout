c600643f293c66c777b8be8812ef7a62778f9614
zhilyakov1986 <zhilyakov1986@gmail.com>
2019-10-12 07:22:01 -0400

    data managing

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({ providedIn: "root" })
 export class CartService {
@@ -12,10 +13,14 @@ export class CartService {
     return this.items;
   }
 
-  clearCart(){
+  clearCart() {
     this.items = [];
     return this.items;
   }
 
-  constructor() {}
+  getShippingPrices() {
+    return this.http.get("/assets/shipping.json");
+  }
+
+  constructor(private http: HttpClient) {}
 }
